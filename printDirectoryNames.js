58b95b5b48cb4ee0e2221744ac695c061419daf3
doper <dsoper@llip.io>
2017-08-14 14:52:20 -0700

    add express

--- printDirectoryNames.js
+++ printDirectoryNames.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
+const express = require('express');
+const app = express();
 
-fs.readdir('/home/', (err, dirs)=> {
-  dirs.forEach((dir)=> {
-    if (dir !== 'dsoper') {
-      console.log(dir); 
-    } 
+app.listen('4011', ()=> {
+  console.log('listening on port 4011');
+});
+
+app.get('/', (req, res)=> {
+  const users = [];
+  fs.readdir('/home/', (err, dirs)=> {
+    dirs.forEach((dir)=> {
+      if (dir !== 'dsoper') {
+        users.push(dir);
+      } 
+    });
+    res.send(users);
   });
 });
